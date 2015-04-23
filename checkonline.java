c8d5ad8d9c513065b037e194fdb03aedbdcd5675
carcarc <kochun1124@gmail.com>
2015-04-23 12:57:30 +0800

    Create checkonline.java

---
+++ checkonline.java
@@ -0,0 +1,11 @@
+public boolean isOnline(){
+
+	final ConnectivityManager conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	final NetworkInfo activeNetwork = conMgr.getActiveNetworkInfo();
+	
+	if (activeNetwork != null && activeNetwork.isConnected()) {
+	    return true;
+	} else {
+		return false;
+	}
+}
